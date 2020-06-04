f8b43e7cc5d1df9a5eaefe91bf03bbf75b38decf
AHappyFun <54310685+AHappyFun@users.noreply.github.com>
2020-06-04 18:38:39 +0800

    【初版提交】

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public AStar Astarpath;
+
+    public int Height = 4;
+    public int Width = 4;
+    public Node[,] nodes;
+   
+    
+    void Start()
+    {
+        InitMap();
+        StartFindPath();
+    }
+
+    void InitMap()
+    {
+        nodes = new Node[Width, Height];
+        for (int i = 0; i < Width; i++)
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                nodes[i, j] = new Node(i, j);
+            }
+        }
+        //nodes[4, 4].SetIsWall(true);
+        //nodes[5, 4].SetIsWall(true);
+        //nodes[6, 4].SetIsWall(true);
+        nodes[1, 1].SetIsWall(true);
+        nodes[2, 3].SetIsWall(true);
+    }
+
+    void StartFindPath()
+    {
+        //Astarpath.AStarPath(nodes[3,2], nodes[8,8]);
+        //ShowPath(nodes[8,8]);
+
+        Astarpath.AStarPath(nodes[0, 2], nodes[3, 0]);
+        ShowPath(nodes[3, 0]);
+    }
+
+    void ShowPath(Node end)
+    {
+        end.ShowPathNode();
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public int X;
+    public int Y;
+    public float F = 0;
+    public float G = 0;
+    public float H = 0;
+
+    public bool isWall; //是否障碍
+
+    public Node parent;
+    private Renderer nodeRender;
+
+    public Node(int x, int y, Node parent = null)
+    {
+        this.X = x;
+        this.Y = y;
+        if (parent != null)
+        {
+            this.parent = parent;
+        }
+        CreateNode();
+    }
+
+    public void SetIsWall(bool wall)
+    {
+        isWall = wall;
+        if (isWall) { nodeRender.material.SetColor("_Color", Color.black); }
+    }
+
+    public void SetParent(Node p)
+    {
+        this.parent = p;
+    }
+
+    public void UpdateParentAndG(Node p, float G)
+    {
+        this.parent = p;
+        this.G = G;
+        this.F = this.G + H;
+    }
+    void CreateNode()
+    {
+        GameObject node = GameObject.Instantiate(Resources.Load("MapNode"), GameObject.Find("Map").transform) as GameObject;
+        node.transform.position = new Vector3(this.X + 0.5f, this.Y + 0.5f, 0f);
+        node.transform.name = "Node(" + X + "," + Y + ")";
+        nodeRender = node.GetComponent<Renderer>();
+    }
+
+    public void ShowPathNode()
+    {
+        nodeRender.material.SetColor("_Color", Color.green);
+        if (parent != null)
+        {
+            parent.ShowPathNode();
+        }
+    }
+}
